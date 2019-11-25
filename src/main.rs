01043e38dbb97d35082ed56e4b53d0bee8feee50
Quinn Shanahan <q.shanahan@gmail.com>
2019-11-25 10:56:20 -0500

    interact with api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+#[macro_use]
+extern crate serde;
+extern crate serde_derive;
+extern crate reqwest;
+
+mod api;
+
+use std::error::Error;
+// use std::result::Result;
+// use reqwest::Error;
+use dotenv::dotenv;
+use std::env;
+use serde::Deserialize;
+// use api;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    dotenv().ok();
+
+    let response = api::get_deployment(&"component-library");
+    println!("{:?}", response?.status());
+
+    Ok(())
 }
