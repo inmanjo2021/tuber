3186b7103a56cdd974d7ec529cc052a2c464e644
Quinn Shanahan <q.shanahan@gmail.com>
2019-11-28 10:44:44 -0500

    Download using docker hub format (not available on GCR

--- src/fetch_layer.rs
+++ src/fetch_layer.rs
@@ -1,13 +1,32 @@
+extern crate flate2;
+extern crate tar;
+extern crate hyper;
+extern crate reqwest;
+
 use std::error::Error;
 use serde::{Deserialize, Deserializer};
 use serde::de::{self, Visitor};
 use serde_json;
+use std::fmt;
+use std::env;
+// use std::io::copy;
+// use std::fs::File;
+// use std::path::Path;
+use tar::Archive;
+use flate2::read::GzDecoder;
+use std::io::Read;
+// use hyper::header::{Headers, Authorization, Basic};
+// use reqwest::header::{HeaderMap, Authorization, Basic};
 
-const REGISTRY_BASE:  &str = "https://registry-1.docker.io";
-const AUTH_BASE:      &str = "https://auth.docker.io";
-const AUTH_SERVICE:   &str = "registry.docker.io";
-const IMAGE:          &str = "library/node";
-const TAG:            &str = "latest";
+// const REGISTRY_BASE:  &str = "https://registry-1.docker.io";
+// const AUTH_BASE:      &str = "https://auth.docker.io";
+// const AUTH_SERVICE:   &str = "registry.docker.io";
+
+const REGISTRY_BASE:  &str = "https://us.gcr.io";
+const AUTH_BASE:      &str = "https://us.gcr.io";
+// const AUTH_SERVICE:   &str = "registry.docker.io";
+const IMAGE:          &str = "freshly-docker/address-service";
+const TAG:            &str = "master";
 const MATCHER:        &str = "COPY";
 
 #[derive(Deserialize)]
@@ -50,11 +69,9 @@ fn deserialize_layer_command<'de, D>(deserializer: D) -> Result<String, D::Error
 where
     D: Deserializer<'de>,
 {
-    use std::fmt;
+    struct LayerCommandVisitor;
 
-    struct TimeVisitor;
-
-    impl<'de> Visitor<'de> for TimeVisitor {
+    impl<'de> Visitor<'de> for LayerCommandVisitor {
         type Value = String;
 
         fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -67,15 +84,83 @@ where
             E: de::Error,
         {
             let deserialized: HistoryData = serde_json::from_str(&value).unwrap();
-
             Ok(deserialized.container_config.cmd.join(" ").clone())
         }
     }
 
-    deserializer.deserialize_string(TimeVisitor)
+    deserializer.deserialize_string(LayerCommandVisitor)
 }
 
-pub fn get_layers() -> Result<(), Box<dyn Error>> {
+fn get_token() -> Result<AuthResponse, Box<dyn Error>> {
+    // This is the auth format for docker hub.
+    // Unsure where the distinctions between these two are specified.
+    // let request_url = format!(
+    //     "{auth_base}/token?service={auth_service}&scope=repository:{image}:pull",
+    //     auth_base = AUTH_BASE,
+    //     auth_service = "registry.docker.io",
+    //     image = IMAGE,
+    // );
+
+    // GCP does not use the service query string param.
+    // It also needs basic auth, which makes sense i guess?
+    let request_url = format!(
+        "{auth_base}/v2/token?scope=repository:{image}:pull",
+        auth_base = AUTH_BASE,
+        image = IMAGE,
+    );
+
+    let client = reqwest::Client::new();
+    let mut response = client.get(&request_url)
+        .basic_auth("_token", Some(env::var("GCLOUD_TOKEN")?))
+        .send()?
+        .error_for_status()
+        .unwrap();
+
+    let auth_response: AuthResponse = response.json()?;
+
+    println!("TOKEN IS !!!! : ");
+    println!("{}", auth_response.token);
+
+    Ok(auth_response)
+}
+
+pub fn find_layer() -> Result<String, Box<dyn Error>> {
+    download_layer()
+}
+
+pub fn download_layer() -> Result<String, Box<dyn Error>> {
+    let token = get_token()?.token;
+    let layer = get_layer_sha().unwrap();
+
+    let request_url = format!(
+        "{registry_base}/v2/{image}/blobs/{layer}",
+        image = IMAGE,
+        registry_base = REGISTRY_BASE,
+        layer = layer,
+    );
+
+    let client = reqwest::Client::new();
+
+    let response = client.get(&request_url)
+        .header("Authorization", format!("Bearer {}", token))
+        .send()?;
+
+    let tar = GzDecoder::new(response);
+    let mut archive = Archive::new(tar);
+
+    for file in archive.entries().unwrap() {
+        let mut file = file.unwrap();
+
+        // files implement the Read trait
+        let mut s = String::new();
+        file.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+    }
+
+    Ok(layer)
+}
+
+fn get_layer_sha() -> Result<String, Box<dyn Error>> {
     let token = get_token()?.token;
 
     let request_url = format!(
@@ -87,30 +172,20 @@ pub fn get_layers() -> Result<(), Box<dyn Error>> {
 
     let client = reqwest::Client::new();
 
+    // println!("{}", token);
+
     let mut response = client.get(&request_url)
         .header("Authorization", format!("Bearer {}", token))
-        .send()?;
+        // .header("Accept", "application/vnd.docker.container.image.v1+json")
+        .send()?
+        .error_for_status()
+        .unwrap();
 
-    // print!("{}", response.text()?);
+    println!("{}", response.text().unwrap());
+
     let parsed: Manifest = response.json()?;
     let index = parsed.history.into_iter().position(|x| x.command.contains(MATCHER)).unwrap();
     let layer = parsed.fs_layers[index].blob_sum.clone();
 
-    print!("{}", layer);
-
-    Ok(())
-}
-
-fn get_token() -> Result<AuthResponse, Box<dyn Error>> {
-    let request_url = format!(
-        "{auth_base}/token?service={auth_service}&scope=repository:{image}:pull",
-        auth_base = AUTH_BASE,
-        auth_service = AUTH_SERVICE,
-        image = IMAGE,
-    );
-
-    let mut response = reqwest::get(&request_url)?;
-    let auth_response: AuthResponse = response.json()?;
-
-    Ok(auth_response)
+    Ok(layer)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,10 @@ mod api;
 mod fetch_layer;
 
 use std::error::Error;
-// use std::result::Result;
-// use reqwest::Error;
 use dotenv::dotenv;
-// use std::env;
-// use serde::Deserialize;
-// use api;
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-
-    // let response = api::get_deployment(&"component-library");
-    // println!("{}", response.token);
-    // println!("{:?}", response.text());
-    fetch_layer::get_layers()?;
-
+    fetch_layer::find_layer()?;
     Ok(())
 }
