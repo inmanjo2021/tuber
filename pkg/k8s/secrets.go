28ceac1327cf3866c9b801c35aa700bc9a12f96d
Quinn Shanahan <q.shanahan@gmail.com>
2019-12-13 13:14:25 -0500

    secrets

---
+++ pkg/k8s/secrets.go
@@ -0,0 +1,60 @@
+package k8s
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"tuber/pkg/apply"
+	"tuber/pkg/util"
+)
+
+type metadata struct {
+	Name string `json:"name"`
+}
+
+type secretsConfig struct {
+	APIVersion string            `json:"apiVersion"`
+	Kind       string            `json:"kind"`
+	Metadata   metadata          `json:"metadata"`
+	Type       string            `json:"type"`
+	Data       map[string]string `json:"stringData"`
+}
+
+// CreateFromFile creates a secret based on the contents of a file
+func CreateFromFile(path string, mountpoint string) (dat []byte, err error) {
+	dat, err = ioutil.ReadFile(path)
+	projectName := "tuber"
+
+	if err != nil {
+		return
+	}
+
+	str := string(dat)
+	filename := filepath.Base(path)
+	data := map[string]string{filename: str}
+	meta := metadata{
+		Name: fmt.Sprintf("%s/%s", projectName, filename),
+	}
+
+	config := secretsConfig{
+		APIVersion: "v1",
+		Kind:       "Secret",
+		Type:       "Opaque",
+		Data:       data,
+		Metadata:   meta,
+	}
+
+	var yamls []util.Yaml
+	var jsondata []byte
+
+	json.Unmarshal(jsondata, &config)
+
+	yamls = append(yamls, util.Yaml{
+		Content: string(jsondata),
+	})
+
+	apply.Apply(yamls)
+
+	return
+}
