4c829f5f0f2b67628ed306768264bf74dda2187d
Jordan Minneti <jkminneti@gmail.com>
2020-01-10 11:19:48 -0500

    make core more core to core

--- pkg/core/interpolate.go
+++ pkg/core/interpolate.go
@@ -2,12 +2,11 @@ package core
 
 import (
 	"strings"
-	"tuber/pkg/k8s"
 )
 
 // ReleaseTubers combines and interpolates with tuber's conventions, and applies them
 func ReleaseTubers(tubers []string, app *TuberApp, digest string) ([]byte, error) {
-	return k8s.ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), tuberData(app, digest))
+	return ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), tuberData(app, digest))
 }
 
 func tuberData(app *TuberApp, digest string) (data map[string]string) {
--- pkg/k8s/namespace.go
+++ pkg/k8s/namespace.go
@@ -1,12 +1,8 @@
 package k8s
 
 import (
-	"bytes"
 	"fmt"
 	"os/exec"
-	"text/template"
-
-	"github.com/MakeNowJust/heredoc"
 )
 
 // CreateNamespace create a new namespace in kubernetes
@@ -20,66 +16,4 @@ func CreateNamespace(namespace string) (err error) {
 	}
 
 	return
-}
-
-type templater struct {
-	Namespace string
-}
-
-func ApplyTemplate(namespace string, templatestring string, params map[string]string) (out []byte, err error) {
-	tpl, err := template.New("tpl").Parse(templatestring)
-
-	if err != nil {
-		return
-	}
-
-	var buf bytes.Buffer
-	err = tpl.Execute(&buf, params)
-
-	if err != nil {
-		return
-	}
-
-	out, err = Apply(buf.Bytes(), namespace)
-
-	return
-}
-
-// BindNamespace create a new namespace in kubernetes
-func BindNamespace(namespace string) ([]byte, error) {
-	templatestring := heredoc.Doc(`
-		---
-		kind: Role
-		apiVersion: rbac.authorization.k8s.io/v1beta1
-		metadata:
-		  name: tuber-admin
-		  namespace: {{ .Namespace }}
-		rules:
-		- apiGroups:
-		  - '*'
-		  resources:
-		  - '*'
-		  verbs:
-		  - '*'
-		---
-		kind: RoleBinding
-		apiVersion: rbac.authorization.k8s.io/v1beta1
-		metadata:
-		  name: tuber-admin
-		  namespace: {{ .Namespace }}
-		roleRef:
-		  apiGroup: rbac.authorization.k8s.io
-		  kind: Role
-		  name: tuber-admin
-		subjects:
-		- kind: ServiceAccount
-		  name: default
-		  namespace: tuber
-	`)
-
-	params := map[string]string{
-		"Namespace": namespace,
-	}
-
-	return ApplyTemplate(namespace, templatestring, params)
 }
