e5d1d0aaa2338ac629d1591090071e90fc2d9107
Harrison <hborges005@gmail.com>
2020-01-22 14:03:43 -0500

    Add init command

---
+++ cmd/init.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"tuber/pkg/core"
+
+	"github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init [appName]",
+	Short: "initialize a .tuber directory and relevant yamls",
+	Long:  ``,
+	Args:  cobra.ExactArgs(1),
+	RunE:  initialize,
+}
+
+func initialize(cmd *cobra.Command, args []string) (err error) {
+	appName := args[0]
+
+	if err := core.CreateTuberDirectory(); err != nil {
+		return
+	}
+
+	if err = core.CreateDeploymentYAML(appName); err != nil {
+		return
+	}
+
+	return
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
---
+++ pkg/core/init.go
@@ -0,0 +1,76 @@
+package core
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"text/template"
+
+	"github.com/MakeNowJust/heredoc"
+)
+
+const tuberConfigPath = "./tuber"
+
+var deployment = heredoc.Doc(`
+	apiVersion: apps/v1
+	kind: Deployment
+	metadata:
+		labels:
+			app: {{.appName}}
+		name: {{.appName}}
+		namespace: {{.appName}}
+	spec:
+		replicas: 1
+		selector:
+			matchLabels:
+				app: {{.appName}}
+		template:
+			metadata:
+				labels:
+					app: {{.appName}}
+			spec:
+				containers:
+				- image: {{"{{.tuberImage}}"}}
+					name: {{.appName}}
+					envFrom:
+						- secretRef:
+								name: {{.appName}}-env
+					ports:
+						- containerPort: 80
+`)
+
+func CreateTuberDirectory() (err error) {
+	if _, err := os.Stat(tuberConfigPath); os.IsNotExist(err) {
+		if err := os.Mkdir(tuberConfigPath, os.ModePerm); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func CreateDeploymentYAML(appName string) (err error) {
+	if _, err := os.Stat(tuberConfigPath + "deployment.yaml"); os.IsExist(err) {
+		return nil
+	}
+
+	tpl, err := template.New("tpl").Parse(deployment)
+	if err != nil {
+		return
+	}
+
+	var buff bytes.Buffer
+	templateData := map[string]string{
+		"appName": appName,
+	}
+
+	if err = tpl.Execute(&buff, templateData); err != nil {
+		return
+	}
+
+	if err := ioutil.WriteFile(tuberConfigPath, buff.Bytes(), 0644); err != nil {
+		return
+	}
+
+	return
+}
