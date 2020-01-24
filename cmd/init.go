d11492677cec26c7df1c68d7032617836063806e
Harrison <hborges005@gmail.com>
2020-01-24 17:40:20 -0500

    Generate service.yaml

--- cmd/init.go
+++ cmd/init.go
@@ -7,25 +7,19 @@ import (
 )
 
 var initCmd = &cobra.Command{
-	Use:   "init [appName]",
+	Use:   "init [appName] [portNumber]",
 	Short: "initialize a .tuber directory and relevant yamls",
-	Long:  ``,
-	Args:  cobra.ExactArgs(1),
-	RunE:  initialize,
+	Long: `App name is the name of your app, which will be interpolated into the configuration files and used as the
+		namespace, as well as other things. Port number is the port that your app will run on. The standard is port 80.`,
+	Args: cobra.ExactArgs(2),
+	RunE: initialize,
 }
 
 func initialize(cmd *cobra.Command, args []string) (err error) {
 	appName := args[0]
+	portNumber := args[1]
 
-	if err = core.CreateTuberDirectory(); err != nil {
-		return
-	}
-
-	if err = core.CreateDeploymentYAML(appName); err != nil {
-		return
-	}
-
-	return
+	return core.Init(appName, portNumber)
 }
 
 func init() {
--- pkg/core/init.go
+++ pkg/core/init.go
@@ -39,7 +39,38 @@ var deployment = heredoc.Doc(`
 						- containerPort: 80
 `)
 
-func CreateTuberDirectory() (err error) {
+var service = heredoc.Doc(`
+	apiVersion: v1
+	kind: Service
+	metadata:
+		name: {{.appName}}-service
+		namespace: {{.appName}}
+	spec:
+		ports:
+		- port: 9090
+			targetPort: {{.port}}
+			name: grpc-{{.appName}}
+		selector:
+			app: {{.appName}}
+`)
+
+func Init(appName string, port string) (err error) {
+	if err = createTuberDirectory(); err != nil {
+		return
+	}
+
+	if err = createDeploymentYAML(appName); err != nil {
+		return
+	}
+
+	if err = createServiceYAML(appName, port); err != nil {
+		return
+	}
+
+	return
+}
+
+func createTuberDirectory() (err error) {
 	if err = os.Mkdir(tuberConfigPath, os.ModePerm); os.IsExist(err) {
 		return nil
 	}
@@ -47,22 +78,36 @@ func CreateTuberDirectory() (err error) {
 	return
 }
 
-func CreateDeploymentYAML(appName string) (err error) {
-	tpl, err := template.New("tpl").Parse(deployment)
+func createDeploymentYAML(appName string) (err error) {
+	templateData := map[string]string{
+		"appName": appName,
+	}
+
+	return writeYAML("deployment.yaml", deployment, templateData)
+}
+
+func createServiceYAML(appName string, port string) (err error) {
+	templateData := map[string]string{
+		"appName": appName,
+		"port":    port,
+	}
+
+	return writeYAML("service.yaml", service, templateData)
+}
+
+func writeYAML(fileName string, templateString string, templateData map[string]string) (err error) {
+	tpl, err := template.New("tpl").Parse(templateString)
 	if err != nil {
 		return
 	}
 
 	var buff bytes.Buffer
-	templateData := map[string]string{
-		"appName": appName,
-	}
 
 	if err = tpl.Execute(&buff, templateData); err != nil {
 		return
 	}
 
-	if err = ioutil.WriteFile(tuberConfigPath+"/deployment.yaml", buff.Bytes(), 0644); err != nil {
+	if err = ioutil.WriteFile(tuberConfigPath+"/"+fileName, buff.Bytes(), 0644); err != nil {
 		return
 	}
 
