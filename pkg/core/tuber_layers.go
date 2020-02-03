c8c49106d3b024c073ad2b10ca61e06685dbcc03
Harrison <hborges005@gmail.com>
2020-02-03 17:54:45 -0500

    Remove unused function arg

--- pkg/core/tuber_layers.go
+++ pkg/core/tuber_layers.go
@@ -6,7 +6,7 @@ import (
 
 // ReleaseTubers combines and interpolates with tuber's conventions, and applies them
 func ReleaseTubers(tubers []string, app *TuberApp, digest string, data *ClusterData) ([]byte, error) {
-	return ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), tuberData(app, digest, data))
+	return ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), tuberData(digest, data))
 }
 
 // ClusterData is configurable, cluster-wide data available for yaml interpolation
@@ -15,7 +15,7 @@ type ClusterData struct {
 	DefaultHost    string
 }
 
-func tuberData(app *TuberApp, digest string, clusterData *ClusterData) (data map[string]string) {
+func tuberData(digest string, clusterData *ClusterData) (data map[string]string) {
 	return map[string]string{
 		"tuberImage":            digest,
 		"clusterDefaultGateway": clusterData.DefaultGateway,
