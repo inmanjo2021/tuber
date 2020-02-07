e7c26ef30b616e1b073adbfae23503297eb8895c
Harrison <hborges005@gmail.com>
2020-02-07 16:37:20 -0500

    Refactor init to use pkger

--- pkg/core/init.go
+++ pkg/core/init.go
@@ -6,10 +6,11 @@ import (
 	"os"
 	"text/template"
 
-	"github.com/gobuffalo/packr"
+	"github.com/markbates/pkger"
 )
 
 const tuberConfigPath = ".tuber"
+const templateBasePath = "/yamls"
 
 // InitTuberApp creates a bunch of yamls for you
 func InitTuberApp(appName string, routePrefix string) (err error) {
@@ -17,32 +18,15 @@ func InitTuberApp(appName string, routePrefix string) (err error) {
 		return
 	}
 
-	box := packr.NewBox("../../yamls")
-
-	deploymentYaml, err := box.FindString("deployment.yaml")
-	if err != nil {
+	if err = createDeploymentYAML(appName); err != nil {
 		return
 	}
 
-	if err = createDeploymentYAML(appName, deploymentYaml); err != nil {
+	if err = createServiceYAML(appName); err != nil {
 		return
 	}
 
-	serviceYaml, err := box.FindString("service.yaml")
-	if err != nil {
-		return
-	}
-
-	if err = createServiceYAML(appName, serviceYaml); err != nil {
-		return
-	}
-
-	virtualServiceYaml, err := box.FindString("virtual_service.yaml")
-	if err != nil {
-		return
-	}
-
-	if err = createVirtualServiceYAML(appName, routePrefix, virtualServiceYaml); err != nil {
+	if err = createVirtualServiceYAML(appName, routePrefix); err != nil {
 		return
 	}
 
@@ -57,33 +41,46 @@ func createTuberDirectory() (err error) {
 	return
 }
 
-func createDeploymentYAML(appName string, fileContent string) (err error) {
+func createDeploymentYAML(appName string) (err error) {
 	templateData := map[string]string{
 		"appName": appName,
 	}
 
-	return writeYAML("deployment.yaml", fileContent, templateData)
+	return writeYAML("deployment.yaml", templateData)
 }
 
-func createServiceYAML(appName string, fileContent string) (err error) {
+func createServiceYAML(appName string) (err error) {
 	templateData := map[string]string{
 		"appName": appName,
 	}
 
-	return writeYAML("service.yaml", fileContent, templateData)
+	return writeYAML("service.yaml", templateData)
 }
 
-func createVirtualServiceYAML(appName string, routePrefix string, fileContent string) (err error) {
+func createVirtualServiceYAML(appName string, routePrefix string) (err error) {
 	templateData := map[string]string{
 		"appName":     appName,
 		"routePrefix": routePrefix,
 	}
 
-	return writeYAML("virtual_service.yaml", fileContent, templateData)
+	return writeYAML("virtual_service.yaml", templateData)
 }
 
-func writeYAML(fileName string, templateString string, templateData map[string]string) (err error) {
-	tpl, err := template.New("tpl").Parse(templateString)
+func writeYAML(fileName string, templateData map[string]string) (err error) {
+	templateDir := pkger.Dir("/yamls")
+
+	templateFile, err := templateDir.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer templateFile.Close()
+
+	templateFileBytes, err := ioutil.ReadAll(templateFile)
+	if err != nil {
+		return
+	}
+
+	tpl, err := template.New("tpl").Parse(string(templateFileBytes))
 	if err != nil {
 		return
 	}
