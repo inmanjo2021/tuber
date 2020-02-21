6db6b1768487e134a9a3f0f30f3013d51c61017b
Jordan Minneti <jkminneti@gmail.com>
2020-02-21 15:43:35 -0500

    idempotency or something (#82)
    * idempotency or something
    
    * friendly spacing
    
    * just kubectl get a resource

--- pkg/core/create_tuber_app.go
+++ pkg/core/create_tuber_app.go
@@ -1,33 +1,50 @@
 package core
 
 import (
+	"fmt"
 	data "tuber/data/tuberapps"
 	"tuber/pkg/k8s"
 )
 
 // CreateTuberApp adds a new tuber app configuration, including namespace,
 // role, rolebinding, and a listing in tuber-apps
-func CreateTuberApp(appName string, repo string, tag string) (err error) {
+func CreateTuberApp(appName string, repo string, tag string) error {
 	namespaceData := map[string]string{
 		"namespace": appName,
 	}
 
+	existsAlready, err := k8s.Exists("namespace", appName, appName)
+	if err != nil {
+		return err
+	}
+
+	if existsAlready {
+		return AddAppConfig(appName, repo, tag)
+	} else {
+		err := newAppSetup(appName, namespaceData)
+		if err != nil {
+			return err
+		}
+		return AddAppConfig(appName, repo, tag)
+	}
+}
+
+func newAppSetup(appName string, namespaceData map[string]string) error {
+	var err error
 	for _, yaml := range []data.TuberYaml{data.Namespace, data.Role, data.Rolebinding} {
 		err = ApplyTemplate(appName, string(yaml.Contents), namespaceData)
-		if err != nil {
-			return
+	}
+	if err == nil {
+		err = k8s.CreateEnv(appName)
+	}
+
+	if err != nil {
+		deleteErr := k8s.Delete("namespace", appName, appName)
+		if deleteErr != nil {
+			return fmt.Errorf(err.Error() + " failed delete: " + deleteErr.Error())
 		}
+		return err
 	}
 
-	err = k8s.CreateEnv(appName)
-
-	if err != nil {
-		return
-	}
-
-	if err != nil {
-		return
-	}
-
-	return AddAppConfig(appName, repo, tag)
+	return nil
 }
--- pkg/k8s/kubectl.go
+++ pkg/k8s/kubectl.go
@@ -85,3 +85,17 @@ func Restart(resource string, namespace string, args ...string) (err error) {
 	_, err = kubectl(append(restart, args...)...)
 	return
 }
+
+// Exists tells you if a given resource already exists. Errors if a get call fails for any reason other than Not Found
+func Exists(kind string, name string, namespace string, args ...string) (bool, error) {
+	get := []string{"get", kind, name, "-n", namespace}
+	_, err := kubectl(append(get, args...)...)
+	if err, ok := err.(NotFoundError); ok {
+		if ok {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, nil
+}
