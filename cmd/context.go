c85396698f54aad7a14cbd14325aa2aef41cb104
Jordan Minneti <jkminneti@gmail.com>
2021-06-02 09:25:53 -0700

    context command (#166)
    * canaries
    
    * undo the refactor that mysteriously broke everything
    
    * like why doesnt this work (#165)
    
    * like why doesnt this work
    
    * Fix return values for convertResponse
    
    Co-authored-by: jaysonesmith <gh@nes.33mail.com>
    
    * yeah that's clearer
    
    * so you dont have to remember kubectl config current-context
    
    * don't error if you don't have tuber config set up
    
    * Update context.go
    
    Co-authored-by: jaysonesmith <gh@nes.33mail.com>

---
+++ cmd/context.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/freshly/tuber/pkg/k8s"
+
+	"github.com/spf13/cobra"
+)
+
+var contextCmd = &cobra.Command{
+	SilenceUsage: true,
+	Use:          "context",
+	Short:        "displays current context",
+	RunE:         currentContext,
+}
+
+func currentContext(*cobra.Command, []string) error {
+	currentCluster, err := k8s.CurrentCluster()
+	if err != nil {
+		return err
+	}
+
+	config, err := getTuberConfig()
+	if err != nil {
+		fmt.Println(currentCluster)
+		return nil
+	}
+
+	if config == nil {
+		fmt.Println(currentCluster)
+		return nil
+	}
+
+	cluster := config.FindByName(currentCluster)
+
+	if cluster.Name == "" {
+		fmt.Println(currentCluster)
+	} else {
+		fmt.Println(cluster.Shorthand)
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(contextCmd)
+}
