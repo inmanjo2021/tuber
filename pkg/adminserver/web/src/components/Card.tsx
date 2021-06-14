7cb62a11520e169509e2f892ad0a24bbd4b54104
Quinn Shanahan <quinn.shanahan@freshly.com>
2021-06-13 23:27:59 -0400

    oops

---
+++ pkg/adminserver/web/src/components/Card.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Card = ({ children, className = '' }) => {
+    return <div className={`${className} bg-white dark:bg-gray-800 block p-4 leading-4`}>
+        {children}
+    </div>
+}
